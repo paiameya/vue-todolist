366332e9f2163501795dca0e3056cfec20b1aed2
paiameya <ameya@rayabharitech.com>
2021-05-03 15:30:38 +0530

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,14 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to Your Vue.js App" />
+  <Todolist />
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import Todolist from "./components/Todolist.vue";
 
 export default {
   name: "App",
   components: {
-    HelloWorld,
+    Todolist,
   },
 };
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/components/Todolist.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="todolist"></div>
+</template>
+
+<script>
+export default {
+  name: "Todolist",
+};
+</script>
+
+<style></style>
